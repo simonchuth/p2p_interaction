bc86c9890e6d2feed9506cfb8acfa49455e3bc99
Simon Chu <simon_chu@kelaberetiv.sg>
2020-11-20 15:51:02 +0800

    fix bug with numpy

--- src/model.py
+++ src/model.py
@@ -36,7 +36,7 @@ class ProteinInteraction(nn.Module):
 
 
 def evaluate(target, prediction):
-    target = np.argmax(target, dim=1)
+    target = np.argmax(target, axis=1)
 
     accuracy = accuracy_score(target, prediction)
     f1 = f1_score(target, prediction)
